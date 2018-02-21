6cee74d21d4b8682f7e48aff2bd4630c57132cd2
Joe-noh <goflb.jh@gmail.com>
2018-02-21 22:41:28 +0900

    mix new notification

---
+++ apps/notification/lib/notification.ex
@@ -0,0 +1,18 @@
+defmodule Notification do
+  @moduledoc """
+  Documentation for Notification.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Notification.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
