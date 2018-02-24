f5d7b08d710709daa8e522fcf3b447098112340e
Joe-noh <goflb.jh@gmail.com>
2018-02-25 02:20:52 +0900

    Implement mutation which creates a user
    finally we can signup

--- apps/api/lib/api_web/schema.ex
+++ apps/api/lib/api_web/schema.ex
@@ -12,6 +12,16 @@ defmodule APIWeb.Schema do
         {:ok, current_user}
       end
     end
+
+    mutation do
+      field :create_user, :user do
+        arg :name, non_null(:string)
+        arg :email, non_null(:string)
+        arg :password, non_null(:string)
+
+        resolve &APIWeb.UserResolver.create_user/2
+      end
+    end
   end
 
   object :user do
