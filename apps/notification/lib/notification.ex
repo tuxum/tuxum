8120abff39f3dfcb80c7521188c104be0a55e23f
Joe-noh <goflb.jh@gmail.com>
2018-08-15 03:27:52 +0900

    remove tests that don't make sense

--- apps/notification/lib/notification.ex
+++ apps/notification/lib/notification.ex
@@ -1,18 +1,2 @@
 defmodule Notification do
-  @moduledoc """
-  Documentation for Notification.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Notification.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
