1d4cbdcc7abc3ec8fb37f6b59a45742e3237b4d2
Joe-noh <goflb.jh@gmail.com>
2018-08-15 03:43:02 +0900

    update phoenix

--- apps/api/mix.exs
+++ apps/api/mix.exs
@@ -36,7 +36,7 @@ defmodule API.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, "~> 1.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
