2b779ac1cdcd2c4a1e4471bbd51d0f8024a98565
Joe-noh <goflb.jh@gmail.com>
2018-08-15 03:46:26 +0900

    update mix_test_watch

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Tuxum.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     [
-      {:mix_test_watch, "~> 0.5", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
     ]
   end
 
